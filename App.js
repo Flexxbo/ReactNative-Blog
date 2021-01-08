f154e1d1db52ea0917ce3c8d39459ccc7722c364
Flexxbo <flexxbo@gmail.com>
2021-01-08 23:36:37 +0100

    error fixes

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+
 import IndexScreen from "./src/screens/IndexScreen";
-import { BlogProvider } from "./src/context/BlogContext";
+import { Provider } from "./src/context/BlogContext";
 
 const navigator = createStackNavigator(
   {
@@ -17,10 +18,11 @@ const navigator = createStackNavigator(
 );
 
 const App = createAppContainer(navigator);
+
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
